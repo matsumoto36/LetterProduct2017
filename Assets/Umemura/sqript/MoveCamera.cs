6333e06cb5398df0edbee142d29e558d0349e7a2
Ha-merun <nkc.yasuo.umemura@gmail.com>
2017-12-19 10:56:18 +0900

    シーン還移ができました

---
+++ Assets/Umemura/sqript/MoveCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("Player1"))
+        {
+            FadeManager.Instance.LoadScene("Camera", 2.0f);
+        }
+    }
+}
