8bb161235318416f33241ec3347384df32f9f866
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-12-21 10:43:27 +0900

    パーティクルと弾の連携・ポストプロセスの設定

--- Assets/Matsumoto/Scripts/System/ParticleManager.cs
+++ Assets/Matsumoto/Scripts/System/ParticleManager.cs
@@ -78,6 +78,8 @@ public sealed class ParticleManager : SingletonMonoBehaviour<ParticleManager> {
 			return null;
 		}
 
+		Debug.Log("Spawn:" + particleName);
+
 		var pObj = Instantiate(pData, position, rotation);
 		pObj.StartEffect();
 
