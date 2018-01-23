85a502047ba13f2c57b3335c3334eaf7d9ee6030
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2018-01-23 14:39:53 +0900

    コントローラーで操作できるボタンのシステムを追加

---
+++ Assets/Matsumoto/Scripts/Test/TestGenerateButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGenerateButton : MonoBehaviour {
+
+	public ControlButton initSelectButton;
+
+	// Use this for initialization
+	void Start() {
+		//InputManager.SetControllerData(0, ControlType.GamePadXBOX, GamepadInput.GamePad.Index.One);
+		var controller = ControlButtonController.CreateController(-1);
+		controller.Focus(initSelectButton);
+	}
+
+}
