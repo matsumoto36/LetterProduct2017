3a82ff78e289e42de22fc92a7a8f5d5b5d92e52f
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2018-01-29 16:34:26 +0900

    入力情報を上書きできる

---
+++ Assets/Matsumoto/Scripts/Debug/EditControllerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class EditControllerInput : MonoBehaviour {
+
+	public int playerNum;
+	public ControlType controllerType;
+	public GamePad.Index controllerIndex;
+
+	public bool setButton;
+
+	void Update() {
+		if(setButton) SetController();
+	}
+
+	void SetController() {
+		setButton = false;
+		Debug.Log(string.Format("SetControllerData [{0}P : ({1}, joystick{2})]", playerNum, controllerType, (int)controllerIndex));
+		InputManager.SetControllerData(playerNum, controllerType, controllerIndex);
+	}
+	
+}
