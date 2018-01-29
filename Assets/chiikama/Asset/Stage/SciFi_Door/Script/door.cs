6e9331c2b59dd1a64474397804f4d19b7d821e7a
eru <eru.birdsea@gmail.com>
2018-01-29 12:22:44 +0900

    全ステージ７×７、ドア制御

--- Assets/chiikama/Asset/Stage/SciFi_Door/Script/door.cs
+++ Assets/chiikama/Asset/Stage/SciFi_Door/Script/door.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
-public class door : MonoBehaviour {
+public class door : MonoBehaviour
+{
 	GameObject thedoor;
 
-void OnTriggerEnter ( Collider obj  ){
-	thedoor= GameObject.FindWithTag("SF_Door");
-	thedoor.GetComponent<Animation>().Play("open");
-}
-
-void OnTriggerExit ( Collider obj  ){
-	thedoor= GameObject.FindWithTag("SF_Door");
-	thedoor.GetComponent<Animation>().Play("close");
-}
+	void Update()
+	{
+		int enemyCount = Unit.unitList
+			.Where(unit => unit.group != UnitGroup.Player)
+			.Count();
+		if (enemyCount <= 0)
+		{
+			thedoor.GetComponent<Animation>().Play("open");
+		}
+	}
 }
