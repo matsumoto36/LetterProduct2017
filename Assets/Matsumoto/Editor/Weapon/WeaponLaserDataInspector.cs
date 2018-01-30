c2c765b0c98b4e843ecefbd4b1aeeec35ca588f8
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2018-01-30 10:17:38 +0900

    レーザー照射中の速度を変更できる

--- Assets/Matsumoto/Editor/Weapon/WeaponLaserDataInspector.cs
+++ Assets/Matsumoto/Editor/Weapon/WeaponLaserDataInspector.cs
@@ -5,11 +5,13 @@ using UnityEditor;
 public class WeaponLaserDataInspector : WeaponRangedDataInspector {
 
 	SerializedProperty chargeSound;
+	SerializedProperty shootingMoveMul;
 
 	protected override void OnEnable() {
 		base.OnEnable();
 
 		chargeSound = serializedObject.FindProperty("chargeSound");
+		shootingMoveMul = serializedObject.FindProperty("shootingMoveMul");
 	}
 
 	public override void OnInspectorGUI() {
@@ -22,7 +24,9 @@ public class WeaponLaserDataInspector : WeaponRangedDataInspector {
 		EditorGUILayout.PropertyField(power, new GUIContent("一秒に与えるダメージ"));
 		EditorGUILayout.PropertyField(mod, new GUIContent("パッシブ効果"), true);
 		EditorGUILayout.PropertyField(bulletData, new GUIContent("照射する弾のデータ"));
-
+		EditorGUILayout.Separator();
+		EditorGUILayout.PropertyField(shootingMoveMul, new GUIContent("撃つときの移動速度の倍率"));
+		EditorGUILayout.Separator();
 		EditorGUILayout.PropertyField(equipSound, new GUIContent("持ち替えた時のSE"));
 		EditorGUILayout.PropertyField(useSound, new GUIContent("発射したときのSE"));
 		EditorGUILayout.PropertyField(hitSound, new GUIContent("当たったときのSE"));
