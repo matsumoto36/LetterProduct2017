677ef79a7824cb018acd98e7cdf4b87a1267b34a
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2018-02-05 13:48:13 +0900

    装備中の武器がわかる

---
+++ Assets/Matsumoto/Scripts/UI/UIPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayer : MonoBehaviour {
+
+	[SerializeField] Text levelText;
+
+	[SerializeField] Image hpBar;
+	[SerializeField] Image expBar;
+
+	[SerializeField] Image inventoryMain;
+	[SerializeField] Image inventorySub;
+
+	[SerializeField] Image warningImage;
+
+	Coroutine warningAnim = null;
+
+	public void SetLevel(int level) {
+		levelText.text = level.ToString();
+	}
+
+	public void SetHPRatio(float hpRatio) {
+		hpBar.fillAmount = hpRatio;
+	}
+
+	public void SetEXPRatio(float expRatio) {
+		expBar.fillAmount = expRatio;
+	}
+
+	public void SetWeaponIcon(Sprite main, Sprite sub) {
+		inventoryMain.sprite = main;
+		inventorySub.sprite = sub;
+	}
+
+	public void SwitchWarningAnim(bool enable) {
+
+		if(enable == (warningAnim != null)) return;
+
+		if(enable) {
+			warningAnim = StartCoroutine(WarningAnim());
+		}
+		else {
+			StopCoroutine(warningAnim);
+			warningAnim = null;
+			warningImage.color = new Color(1, 1, 1, 0);
+		}
+	}
+
+	IEnumerator WarningAnim() {
+
+		var t = 0.0f;
+		while(true) {
+
+			t += Time.deltaTime;
+			var col = warningImage.color;
+			col.a = Mathf.Abs(Mathf.Sin(t * 8));
+			warningImage.color = col;
+
+			yield return null;
+		}
+
+	}
+}
--- Assets/chiikama/Script/HPbar.cs
+++ Assets/chiikama/Script/HPbar.cs
@@ -4,88 +4,53 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HPbar : MonoBehaviour
-{
+public class HPbar : MonoBehaviour {
 
-	public GameObject[] PlayerUI;
-	public Image[] warningImage;
-	public Image[] HPBarArray;
-	public Image[] ExpBarArray;
-	public Text[] LevelText;
-
+	public UIPlayer[] PlayerUI;
 	Player[] playerArray;
-	Coroutine[] warningAnim;
 
-	public void Init()
-	{
+	public void Init() {
 		playerArray = new Player[4];
-		warningAnim = new Coroutine[4];
 
 		GetPlayerArray();
 
-		for (int i = 0; i < 4; i++)
-		{
-			PlayerUI[i].SetActive(playerArray[i]);
+		for(int i = 0;i < 4;i++) {
+			PlayerUI[i].gameObject.SetActive(playerArray[i]);
 		}
 	}
 
-	void GetPlayerArray()
-	{
+	void GetPlayerArray() {
 		var playerList = Unit.unitList
 			.Where(item => item)
 			.Where(item => item is Player)
 			.Select(item => (Player)item)
 			.ToArray();
 
-		foreach (var item in playerList)
-		{
+		foreach(var item in playerList) {
 			playerArray[item.playerIndex] = item;
 		}
 	}
-	void Update()
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			if (playerArray[i] == false) continue;
+
+	void Update() {
+		for(int i = 0;i < 4;i++) {
+			if(playerArray[i] == false) continue;
 
 			var ratio = playerArray[i].HPRatio;
-			if(ratio < GameBalance.instance.data.duraEggCanUseRatio) {
-				if(warningAnim[i] == null) warningAnim[i] = StartCoroutine(WarningAnim(warningImage[i]));
-			}
-			else {
-				if(warningAnim[i] != null) {
-					StopCoroutine(warningAnim[i]);
-					warningImage[i].color = new Color(1, 1, 1, 0);
-				}
-			}
+			PlayerUI[i].SwitchWarningAnim(ratio < GameBalance.instance.data.duraEggCanUseRatio);
 
-			HPBarArray[i].fillAmount = playerArray[i].HPRatio;
-			ExpBarArray[i].fillAmount = playerArray[i].EXPRatio;
-			LevelText[i].text = playerArray[i].level.ToString();
+			PlayerUI[i].SetHPRatio(ratio);
+			PlayerUI[i].SetEXPRatio(playerArray[i].EXPRatio);
+			PlayerUI[i].SetLevel(playerArray[i].level);
+
+			var weaponList = playerArray[i].equipWeapon;
+			PlayerUI[i].SetWeaponIcon(weaponList[0].icon, weaponList[1].icon);
 		}
 	}
 
 	public void OnHide() {
 
-		StopAllCoroutines();
-
-		foreach(var item in warningImage) {
-			item.color = new Color(1, 1, 1, 0);
+		foreach(var item in PlayerUI) {
+			item.SwitchWarningAnim(false);
 		}
-	}
-
-	IEnumerator WarningAnim(Image image) {
-
-		var t = 0.0f;
-		while(true) {
-
-			t += Time.deltaTime;
-			var col = image.color;
-			col.a = Mathf.Abs(Mathf.Sin(t * 8));
-			image.color = col;
-
-			yield return null;
-		}
-
 	}
 }
